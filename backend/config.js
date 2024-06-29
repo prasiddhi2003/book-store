527c15be8409156a9b33447a20d3dc4ff44c04b5
PRASIDDHI <pdm@example.com>
2024-06-29 19:50:56 +0530

    deploy

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+
+export const mongoDBURL='mongodb+srv://root:root@cluster0.ir0affh.mongodb.net/';  
---
+++ backend/index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+
+ 
+
+//middleware for parsing request body
+app.use(express.json());
+
+
+
+
+//middleware for handling CORS POLICY
+//option-1:allow all origins with default of cors(*)
+app.use(cors());
+//option-2:allow custom origins
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Content-Type'],
+//     })
+// );
+
+
+
+
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('welcome to MERN stack tutorial')
+});
+
+
+
+
+
+app.use('/books',booksRoute);
+
+
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("app connected to database");
+        app.listen(PORT, () => {
+            console.log(`app is listening to port:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
